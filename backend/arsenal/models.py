75fff729264516c71f980c1802d80365833938ad
HuangDz <Dzreal_93@126.com>
2019-07-13 11:52:14 +0800

    类别、网站默认显示

--- backend/arsenal/models.py
+++ backend/arsenal/models.py
@@ -35,7 +35,7 @@ class Category(models.Model):
     sn = models.CharField(blank=True, null=True, max_length=20, verbose_name="随机英文ID",
                           help_text="随机英文ID")
     weight = models.IntegerField(default=0, blank=True, verbose_name="权重", help_text="权重")
-    cat_is_show = models.BooleanField(default=False, verbose_name="首页展示", help_text="类别需要首页展示")
+    cat_is_show = models.BooleanField(default=True, verbose_name="首页展示", help_text="类别需要首页展示")
     add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
 
     def __str__(self):
@@ -56,7 +56,7 @@ class Website(models.Model):
     logo = models.ImageField(max_length=200, upload_to="logo", null=True, blank=True)
     category = models.ForeignKey(Category, related_name="website_category", on_delete=models.CASCADE,
                                  verbose_name="类别", help_text="类别")
-    ws_is_show = models.BooleanField(default=False, verbose_name="首页展示", help_text="网站需要首页展示")
+    ws_is_show = models.BooleanField(default=True, verbose_name="首页展示", help_text="网站需要首页展示")
     add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
 
     def __str__(self):
